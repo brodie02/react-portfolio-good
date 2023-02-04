0a4e8642fdb87becbfe9799ce741bd852139e1f0
brodie02 <marshallbrodie0@gmail.com>
2023-02-04 14:53:09 +1030

    Add renderPage function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React, { useState } from 'react'
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import About from './components/pages/About';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/Contact';
+import Resume from './components/pages/Resume';
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('About')
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />
+    }
+    return <Resume />
+  }
+
   return (
     <div className="App">
       <Header />
