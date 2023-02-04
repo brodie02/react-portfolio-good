31de5e971bd5df59d2429a4c641c9f0d152bd0e7
brodie02 <marshallbrodie0@gmail.com>
2023-02-04 17:27:37 +1030

    Add button to hello page

--- src/components/pages/Hello.js
+++ src/components/pages/Hello.js
@@ -4,6 +4,7 @@ export default function Hello() {
     return (
         <div className='hello'>
             <p>Hello! My name is <br></br><span>Brodie Marshall</span><br></br>I am a student full stack web developer</p>
+            <button>View Portfolio</button>
         </div>
     )
 }
