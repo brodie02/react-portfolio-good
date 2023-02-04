4f4af995b6ecbbd81f63a64bd95ae13cb8e4f864
brodie02 <marshallbrodie0@gmail.com>
2023-02-04 21:28:16 +1030

    Add mapping project function

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Project from '../Project'
 
 import techBlog from '../images/techBlog.png'
+import weatherDash from '../images/weather.png'
 
 export default function Portfolio () {
     const projects = [
@@ -12,10 +13,23 @@ export default function Portfolio () {
             tech: "Technologies: JavaScript, Node.js, Handlebars, Sequelize, MySQL, Express, HTML, CSS",
             app: "https://brodie-tech-blog.herokuapp.com/",
             github: "https://github.com/brodie02/tech-blog"
-        }
+        },
+        {
+            title: 'Weather Dashboard',
+            image: weatherDash,
+            body: "This Weather Dashboard collects data from the OpenWeather API depending on what city the user inputs. Th application will then present the current temperature, wind speed, humidity and UV index. It will also show the weather for the next 5 days.",
+            tech: "Technologies: JavaScript, HTML, CSS, OpenWeather API",
+            app: "https://brodie02.github.io/weather-dashboard/",
+            github: "https://github.com/brodie02/weather-dashboard"
+        },
     ]
 
     return (
-        <Project title={projects[0].title} image={projects[0].image}/>
+        // <Project title={projects[0].title} image={projects[0].image} body={projects[0].body} tech={projects[0].tech} app={projects[0].app} github={projects[0].github}/>
+        <div>
+            {projects.map(project => (
+                <Project title={project.title} image={project.image} body={project.body} tech={project.tech} app={project.app} github={project.github}/>
+            ))}
+        </div>
     )
 }
