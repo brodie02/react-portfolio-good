3733d19ddc1d28bdc00ef282ac8e1ec2da991b4f
brodie02 <marshallbrodie0@gmail.com>
2023-02-04 17:38:56 +1030

    Add working links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navigation from './Navigation'
 
-export default function Header() {
+export default function Header({setCurrentPage}) {
     return (
         <header>
-            <h1>Brodie Marshall</h1>
-            <Navigation />
+            <h1><a href='/#' onClick={() => setCurrentPage("Hello")}>Brodie Marshall</a></h1>
+            <Navigation setCurrentPage={setCurrentPage}/>
         </header>
     )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Navigation () {
+export default function Navigation ({setCurrentPage}) {
     return (
         <ul className='nav'>
-            <li><a href='#about'>About Me</a></li>
+            <li><a href='#about' onClick={() => setCurrentPage("About")}>About Me</a></li>
             <li><a href='#portfolio'>Portfolio</a></li>
             <li><a href='#contact'>Contact</a></li>
             <li><a href='#resume'>Resume</a></li>
