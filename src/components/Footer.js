000896c9742fc5b84a2be065cd04955008b1a820
brodie02 <marshallbrodie0@gmail.com>
2023-02-05 13:54:00 +1030

    Add instagram link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react'
+
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram'
 import MailIcon from '@mui/icons-material/Mail';
 
 export default function Footer() {
@@ -9,6 +11,7 @@ export default function Footer() {
             <ul>
                 <li><a href='https://github.com/brodie02'><GitHubIcon fontSize='large'/></a></li>
                 <li><a href='https://www.linkedin.com/in/brodie-marshall-24059a264/'><LinkedInIcon fontSize='large'/></a></li>
+                <li><a href='https://www.instagram.com/brodiesmarshall/?hl=en'><InstagramIcon fontSize='large'/></a></li>
                 <li><a href="mailto: marshallbrodie0@gmail.com"><MailIcon fontSize='large'/></a></li>
             </ul>
         </div>
