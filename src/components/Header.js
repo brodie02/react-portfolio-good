8c341e2c28a2c2c29f58dd23c7104579d8a8e538
brodie02 <marshallbrodie0@gmail.com>
2023-02-05 15:02:25 +1030

    bug fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Navigation from './Navigation'
 export default function Header({currentPage, setCurrentPage}) {
     return (
         <header>
-            <h1><a href='/#' onClick={() => setCurrentPage("Hello")}>Brodie Marshall</a></h1>
+            <h1 onClick={() => setCurrentPage("Hello")}>Brodie Marshall</h1>
             <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage}/>
         </header>
     )
