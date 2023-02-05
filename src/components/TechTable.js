82671fb3cd5b8d2954d6bcd6d6ce8d538afb3918
brodie02 <marshallbrodie0@gmail.com>
2023-02-05 13:34:11 +1030

    Add all logos

--- src/components/TechTable.js
+++ src/components/TechTable.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function TechTable ({title, logos}) {
     return (
         <div className='table'>
+            <div className='table-title'>
                 <h4>{title}</h4>
+            </div>
             <div className='table-pics'>
                 {logos.map(logo => (
                     <img alt='logo' src={logo}></img>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,13 +8,28 @@ import css from '../../assets/logos/css.png'
 import react from '../../assets/logos/react.png'
 import bootstrap from '../../assets/logos/bootstrap.png'
 import jquery from '../../assets/logos/jquery.png'
-
+import node from '../../assets/logos/node.png'
+import express from '../../assets/logos/express.png'
+import sequelize from '../../assets/logos/sequelize.png'
+import mysql from '../../assets/logos/mysql.png'
+import mongo from '../../assets/logos/mongo.png'
+import mongoose from '../../assets/logos/mongoose.png'
+import js from '../../assets/logos/js.png'
+import sql from '../../assets/logos/sql.png'
 
 export default function About() {
     const data = [
         {
             title: "FRONTEND",
             logos: [html, css, react, bootstrap, jquery]
+        },
+        {
+            title: "BACKEND",
+            logos: [node, express, sequelize, mysql, mongo, mongoose]
+        },
+        {
+            title: "LANGUAGES",
+            logos: [js, sql]
         }
     ]
 
@@ -26,6 +41,7 @@ export default function About() {
                 <p>I am an up and coming web developer currently undertaking a Coding Bootcamp through the University of Adelaide. I am enjoying my time as a student and I love learning about web development, it is engaging and the content really sticks with me. This portfolio is my first project made with React. I am constantly studying and coding to expand my knowledge of React and other technologies.</p>
             </div>
             <div className='about-tech'>
+                <h3>Technologies I've Worked With</h3>
                 {data.map(d => (
                     <TechTable title={d.title} logos={d.logos}/>
                 ))}
